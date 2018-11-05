a5c813f121c3c531a6330bff6189736403b9efcc
theinfamousAK <akhilkalepu@protonmail.com>
2018-11-05 14:38:57 -0500

    setup rest of directory, working on orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,19 @@
 // Import MySQL connection
 // =============================================================
 var connection = require("../config/connection.js");
+
+function selectAll() {
+
+}
+
+function insertOne() {
+
+}
+
+function updateOne() {
+
+}
+
+// Export ORM object for model (burger.js)
+// =============================================================
+module.exports = orm;
