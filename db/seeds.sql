d5bff57da0261202e7232662fc2e5a7bbf83cc4d
theinfamousAK <akhilkalepu@protonmail.com>
2018-11-05 14:22:16 -0500

    initial site files, db setup done.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("BBQ Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Vegan Black Bean Burger");
+INSERT INTO burgers (burger_name) VALUES ("Lamb and Feta Burger");
+INSERT INTO burgers (burger_name) VALUES ("Pork Bao Burger");
+
+SELECT * FROM burgers;
